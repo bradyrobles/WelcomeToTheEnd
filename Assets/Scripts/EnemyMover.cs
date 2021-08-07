c00bead1f7a38391fdeaa4e447dd52378debda28
Brady Robles <31491624+bradyrobles@users.noreply.github.com>
2021-08-07 11:15:45 -0400

    cleaned scripts

---
+++ Assets/Scripts/EnemyMover.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    // Parameter Configurations
+    [SerializeField] int x;
+    [SerializeField] int y;
+    [SerializeField] int z;
+
+
+    // Cached References
+    int x1;
+    int x2;
+    int x3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
